0d2e93f8c517fed6444fcbf78af713196b9320a9
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-05-19 01:30:12 -0700

    Add comments for Arjun

--- baseline_metrics/dense_model_benchmark.py
+++ baseline_metrics/dense_model_benchmark.py
@@ -2,6 +2,13 @@
 # https://huggingface.co/meta-llama/Llama-3.2-1B
 # NOTE: this is a gated model, login with a HF token with gated access permission
 
+'''
+Timing seems to exclude memory transfer time or maybe it's simply Python interpreter overhead
+For example, when I ran the prompt, it outputs ~26 seconds but I recorded 45+ seconds on my iPhone
+TODO: test cuda timing
+- Possibly split script into a big if-else statement for CUDA/CPU computation
+'''
+
 from transformers import pipeline
 import torch, time
 from torchvision import models
@@ -14,7 +21,7 @@ tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
 model_name = model.config.name_or_path
 
-if torch.cuda.is_available():   # if we want to save device, set device = torch.device(param)
+if torch.cuda.is_available():
     device = torch.device('cuda')
 else:
     device = torch.device('cpu')
@@ -40,7 +47,7 @@ def generate_response(prompt, max_length=200):
         start = time.perf_counter()
         result = pipe(prompt, max_length=max_length, do_sample=True, temperature=0.7, truncation=True)
         end = time.perf_counter()
-        compute_time = (end - start) # Records in microseconds
+        compute_time = (end - start) 
     return result[0]['generated_text']
     
 test_prompt = "Explain the difference between machine learning and deep learning"
@@ -48,5 +55,5 @@ response = generate_response(test_prompt)
 
 # === Print Results ===
 print(f"\nModel Name: {model_name}")
-print(f"Compute time: {compute_time:.4f} seconds\n")    # Seems to only measure CPU time, excluding tokenization process
+print(f"Compute time: {compute_time:.4f} seconds\n")
 print(f"OUTPUT: \n{response}")
