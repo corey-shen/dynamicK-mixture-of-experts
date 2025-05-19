8d9d0124ed5b30c7641cf2f16c0a46f72d28bd96
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-05-19 00:35:52 -0700

    Add CUDA option and benchmarks for layer, shape, and parameter counts

--- baseline_metrics/dense_model_benchmark.py
+++ baseline_metrics/dense_model_benchmark.py
@@ -3,6 +3,9 @@
 # NOTE: this is a gated model, login with a HF token with gated access permission
 
 from transformers import pipeline
+import torch
+from torchvision import models
+from torchsummary import summary
 
 pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B")
 
@@ -10,6 +13,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
+model_name = model.config.name_or_path
+
+if torch.cuda.is_available():   # if we want to save device, set device = torch.device(param)
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+vgg = models.vgg16().to(device)
+summary(vgg, (3, 224, 224))  # Outputs model's layers, output shapes, and number of parameters
 
 def generate_response(prompt, max_length=200):
     result = pipe(prompt, max_length=max_length, do_sample=True, temperature=0.7, truncation=True)   # do_sample = True selects token based on probability dist., not greedy approach
@@ -17,4 +28,9 @@ def generate_response(prompt, max_length=200):
 
 test_prompt = "Explain the difference between machine learning and deep learning"
 response = generate_response(test_prompt)
-print(response)
+
+
+# === Print Results ===
+print(f"\nModel Name: {model_name}\n")
+print(tokens_per_second)
+print(f"OUTPUT: \n{response}")
