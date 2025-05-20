3af89f2447d0d9734579eaf9f10f1e29a8e8e8f8
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-05-20 00:18:28 -0700

    Add MPS acceleration and kernel caching initialization

--- baseline_metrics/dense_model_benchmark.py
+++ baseline_metrics/dense_model_benchmark.py
@@ -3,8 +3,6 @@
 # NOTE: this is a gated model, login with a HF token with gated access permission
 
 '''
-Timing seems to exclude memory transfer time or maybe it's simply Python interpreter overhead
-For example, when I ran the prompt, it outputs ~26 seconds but I recorded 45+ seconds on my iPhone
 TODO: test cuda timing
 - Possibly split script into a big if-else statement for CUDA/CPU computation
 '''
@@ -15,45 +13,61 @@ from torchvision import models
 from torchsummary import summary
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B")
-
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
 model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
 model_name = model.config.name_or_path
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+    model = model.to(device)
+    print("Using MPS acceleration")
 else:
     device = torch.device('cpu')
-
 compute_time = 0.0
 
-vgg = models.vgg16().to(device)
-summary(vgg, (3, 224, 224))  # Outputs model's layers, output shapes, and number of parameters
+pipe = pipeline("text-generation", model="meta-llama/Llama-3.2-1B", device = device)
 
-def generate_response(prompt, max_length=200):
+vgg_cpu = models.vgg16()
+summary(vgg_cpu, (3, 224, 224), device = "cpu")  # Outputs model's layers, output shapes, and number of parameters
+vgg = vgg_cpu.to(device)    # Set back to device
+
+def generate_response(prompt, max_new_tokens=500): 
     global compute_time
     if device.type == 'cuda':
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
 
         start.record()
-        result = pipe(prompt, max_length=max_length, do_sample=True, temperature=0.7, truncation=True)   # do_sample = True selects token based on probability dist., not greedy approach
+        result = pipe(prompt, max_new_tokens=max_new_tokens, do_sample=True, temperature=0.7, truncation=True)   # do_sample = True selects token based on probability dist., not greedy approach
         end.record()
 
         torch.cuda.synchronize()
-        compute_time = start.elapsed_time(end) / 100
+        compute_time = start.elapsed_time(end) / 1000
     else:
         start = time.perf_counter()
-        result = pipe(prompt, max_length=max_length, do_sample=True, temperature=0.7, truncation=True)
+        result = pipe(prompt, max_new_tokens=max_new_tokens, do_sample=True, temperature=0.7, truncation=True)
         end = time.perf_counter()
         compute_time = (end - start) 
     return result[0]['generated_text']
-    
-test_prompt = "Explain the difference between machine learning and deep learning"
-response = generate_response(test_prompt)
 
-# === Print Results ===
-print(f"\nModel Name: {model_name}")
-print(f"Compute time: {compute_time:.4f} seconds\n")
-print(f"OUTPUT: \n{response}")
+def main():
+    small_prompt = "blah"
+    _ = pipe(small_prompt, max_new_tokens=1, do_sample=False)
+    short_execution = generate_response(small_prompt, 1)
+    print(f"Short execution output to absorb python overhead: {short_execution}")
+
+    test_prompt = "What is the difference between reinforcement learning and machine learning?"
+    response = generate_response(test_prompt)
+    tokens = tokenizer.encode(response)
+    token_count = len(tokens)
+    print(f"Token count: {token_count}")
+
+    # === Print Results ===
+    print(f"\nModel Name: {model_name}")
+    print(f"Compute time: {compute_time:.4f} seconds\n")
+    print(f"OUTPUT: \n{response}")
+
+if __name__ == "__main__":
+    main()
