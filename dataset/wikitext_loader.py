b637adaaabd54bce6bcd2daebd3eb7c78412c401
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-06-28 16:42:45 -0700

    Add GPU utilization

--- dataset/wikitext_loader.py
+++ dataset/wikitext_loader.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
+import torch
 
 # keep in mind that split can be “train” or “validation” or “test”, and the seq_len is the model context length
-def get_wikitext103(split="train", seq_len=1024, batch_size=4, tokenizer=AutoTokenizer.from_pretrained("Qwen/Qwen3-4B")): # optimize/load balancing seq_len via number of heads running
-
+def get_wikitext103(split="train", seq_len=1024, batch_size=4): # optimize/load balancing seq_len via number of heads running
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     data_set = load_dataset("wikitext", "wikitext-103-raw-v1", split=split)
 
     # tokenizes the sample, turning raw strings into input token IDs.
