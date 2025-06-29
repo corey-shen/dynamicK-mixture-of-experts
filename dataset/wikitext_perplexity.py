fc3aa79336156d3b49e9558eca28a6761502915b
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-06-28 18:03:14 -0700

    Change import to class DynamicKGating

--- dataset/wikitext_perplexity.py
+++ dataset/wikitext_perplexity.py
@@ -1,11 +1,11 @@
 import torch, math
-from mixture_of_experts.mixture_of_experts import SimpleLM
+from mixture_of_experts.mixture_of_experts import DynamicKGating
 from wikitext_loader import get_wikitext103
 from transformers import AutoTokenizer
 
 model_id  = "meta-llama/Llama-3.2-1B"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = SimpleLM.load_pretrained(model_id).cuda().eval()
+model = DynamicKGating.load_pretrained(model_id).cuda().eval()
 
 dl = get_wikitext103("test", seq_len=1024, batch_size=4, tokenizer=tokenizer)
 
