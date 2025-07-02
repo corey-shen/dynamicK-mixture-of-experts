b3cec4845b7caf45ac7a0882ecd510f7e7658873
corey-shen <148189230+corey-shen@users.noreply.github.com>
2025-07-01 21:36:31 -0700

    Clean /Mixture_of_experts

--- dataset/Mixture_of_experts/wikitext_perplexity.py
+++ dataset/Mixture_of_experts/wikitext_perplexity.py
@@ -1,17 +1,19 @@
 import torch, math
-print("ABHAHAFHFHFHA")
-from mixture_of_experts import DynamicMoE
-print("XXXXXX")
+from mixture_of_experts import DynamicKGating
 # from wikitext_loader import get_wikitext103
 from transformers import AutoTokenizer
-print("YYYYYYYY")
 from datasets import load_from_disk
-print("ZZZZZZZZZ")
 
+# Training is done here, not mixture_of_experts.py
+# Load in tokenized dataset and model, and then putting in tokenized arrow files
+# Verify dataset through the model, going through dynamic-k algorithm e.g. # transformer heads
+# .no_gradient() at the end
+# Get baseline metrics in
 model_id  = "Qwen/Qwen3-4B"
 
 tokenizer = load_from_disk("tokenized_wikitext103")
-model = DynamicMoE.load_pretrained(model_id).cuda().eval()
+# model = DynamicMoE.load_pretrained(model_id).cuda().eval()
+model = DynamicKGating()
 
 # dl = get_wikitext103("train", seq_len=1024, batch_size=4, tokenizer=tokenizer)
 
